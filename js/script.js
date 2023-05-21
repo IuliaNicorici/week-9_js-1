1bf31aa055eaad993f8631d9b5e10e9a4db2a97c
IuliaNicorici <scorobogatco@gmail.com>
2023-05-21 13:32:49 +0300

    week-9 _js-1

---
+++ js/script.js
@@ -0,0 +1,43 @@
+/* задание 1
+const cleaner = 40000;
+const vocals = 22000;
+console.log (cleaner + vocals);*/ 
+
+/* задание 2
+const num = 2000;
+console.log ('Владивосток '+ num);*/
+
+/* задание 3
+let massenger = 128;
+let foto = 254;
+let vk = 137;
+let youtube = 201;
+let time = massenger + foto + vk + youtube;
+console. log(time);*/
+
+/* задание 4
+console.log(2**5);*/
+
+/* задание 5
+let message = ('"Нужно бежать со всех ног, чтобы только оставаться на месте, а чтобы куда-то попасть, надо бежать как минимум вдвое быстрее!"');
+console.log(message);*/
+
+/* задание 6
+const greetings = 'Привет';
+const name = 'Кот';
+console. log ('Привет' + ',' + 'Кот');*/
+
+/* задание 7
+let time = 34;
+let time2 = time - 15;
+let time3 = time2 - 10;
+let time4 = 0;
+console.log('Старт поездки. Осталось минут: ' + time);
+console.log('Немного сторис в соцсетях. Осталось минут: ' + time2);
+console.log('Немного не новостей, но событий. Осталось минут: ' + time3)
+console.log('Вы приехали. Добро пожаловать в Москву!')*/
+
+/* задание 8
+let fahrenheit = 451;
+let celsius = (fahrenheit - 32) / 1.8;
+console.log(`${fahrenheit} градуса по Фаренгейту — это ${celsius} градуса по Цельсию.`);*/
